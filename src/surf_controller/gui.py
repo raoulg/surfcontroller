90975fc450c3a3f24b7663d9b1267f5ad673c7f6
Raoul Grouls <Raoul.Grouls@han.nl>
2024-08-28 13:18:47 +0200

    move function into class

--- src/surf_controller/gui.py
+++ src/surf_controller/gui.py
@@ -24,63 +24,16 @@ class Controller:
         self.OUTPUT_FILE = self.scriptdir / config["files"]["ids"]
         self.workspace = Workspace()
         self.action = Action()
+        self.vms = self.workspace.get_workspaces(save=True)
+        self.current_row = 0
+        self.selected = [False] * len(self.vms)
+        self.stdscr = None
 
     def __call__(self, stdscr):
-        stdscr.clear()
-        vms = self.workspace.get_workspaces(save=True)
-        selected = [False] * len(vms)
-        current_row = 0
+        self.stdscr = stdscr
+        self.stdscr.clear()
 
-        def print_menu():
-            stdscr.clear()
-            for idx, vm in enumerate(vms):
-                mark = "[*] " if selected[idx] else "[ ] "
-                status = "running" if vm.active else "paused"
-                line = (
-                    mark + vm.name + f"({status})"
-                )  # Combine the mark and the VM name
 
-                if idx == current_row:
-                    stdscr.addstr(
-                        idx, 0, line, curses.A_REVERSE
-                    )  # Highlight the entire line
-                else:
-                    stdscr.addstr(idx, 0, line)  # Display the line without highlighting
-
-            stdscr.addstr(
-                idx + 2,
-                0,
-                "Press \n'j' to move down,\n 'k' to move up,\n'Enter' to select,"
-                "\n 'a' to select all,\n 'p' to pause,\n 'r' to resume,\n 'u' to update status,"
-                "\n's' for ssh access,\n 'l' to toggle logs,\n'q' to quit",
-            )
-            if self.show_logs:
-                stdscr.addstr(len(vms) + 12, 0, "===logs===")
-                for idx, log in enumerate(self.logs[-10:]):
-                    stdscr.addstr(len(vms) + 13 + idx, 0, log)
-            stdscr.refresh()
-
-        def ssh_to_vm(vm):
-            if vm.ip:
-                show_status_message(f"Connecting to {vm.name} at {vm.ip}...")
-                ssh_command = f"ssh {vm.ip}"
-
-                try:
-                    # Use curses.endwin() to temporarily suspend curses
-                    curses.endwin()
-                    subprocess.run(ssh_command, shell=True)
-                except Exception as e:
-                    logger.error(f"SSH connection failed: {str(e)}")
-                finally:
-                    # Reinitialize curses
-                    stdscr.refresh()
-            else:
-                show_status_message(f"No IP address available for {vm.name}")
-
-        def show_status_message(message):
-            stdscr.addstr(len(vms) + 10, 0, message)
-            stdscr.refresh()
-            time.sleep(2)  # Show the message for 2 seconds
 
         def update_logs():
             while True:
@@ -93,56 +46,110 @@ class Controller:
         log_thread = threading.Thread(target=update_logs, daemon=True)
         log_thread.start()
 
-        print_menu()
+        self.print_menu()
 
         while True:
-            key = stdscr.getch()
-            if key == ord("j") and current_row < len(vms) - 1:
-                current_row += 1
-            elif key == ord("k") and current_row > 0:
-                current_row -= 1
+            key = self.stdscr.getch()
+            if key == ord("j") and self.current_row < len(self.vms) - 1:
+                self.current_row += 1
+            elif key == ord("k") and self.current_row > 0:
+                self.current_row -= 1
             elif key == ord("\n"):  # Enter key
-                selected[current_row] = not selected[current_row]
+                self.selected[self.current_row] = not self.selected[self.current_row]
             elif key == ord("a"):  # Select all
-                selected = [True] * len(vms)
+                if all(self.selected):
+                    self.selected = [False] * len(self.vms)
+                else:
+                    self.selected = [True] * len(self.vms)
             elif key == ord("u"):  # Update VM list
-                show_status_message("Updating VM list...\n")
-                vms = self.workspace.get_workspaces(save=False)
+                self.show_status_message("Updating VM list...\n")
+                self.vms = self.workspace.get_workspaces(save=False)
                 logger.info("Updated VM list...")
-                selected = [False] * len(vms)
-                stdscr.refresh()
+                self.selected = [False] * len(self.vms)
+                self.stdscr.refresh()
             elif key == ord("p"):
-                idlist = [vms[i].name for i in range(len(vms)) if selected[i]]
-                show_status_message(f"Pausing {idlist}...\n")
-                stdscr.addstr(2, 0, f"Pausing {idlist}")
-                self.action("pause", vms, idlist)
+                idlist = [self.vms[i].name for i in range(len(self.vms)) if self.selected[i]]
+                self.show_status_message(f"Pausing {idlist}...\n")
+                self.stdscr.addstr(2, 0, f"Pausing {idlist}")
+                self.action("pause", self.vms, idlist)
                 time.sleep(10)
-                vms = self.workspace.get_workspaces(save=False)
-                selected = [False] * len(vms)
-                stdscr.refresh()
+                self.vms = self.workspace.get_workspaces(save=False)
+                self.selected = [False] * len(self.vms)
+                self.stdscr.refresh()
             elif key == ord("r"):  # Resume selected VMs
-                idlist = [vms[i].name for i in range(len(vms)) if selected[i]]
+                idlist = [self.vms[i].name for i in range(len(self.vms)) if self.selected[i]]
                 logger.info(f"Resuming {idlist}...\n")
                 show_status_message(f"Resuming {idlist}...")
 
-                self.action("resume", vms, idlist)
+                self.action("resume", self.vms, idlist)
                 time.sleep(10)
-                vms = self.workspace.get_workspaces(save=False)
-                selected = [False] * len(vms)
-                stdscr.refresh()
+                self.vms = self.workspace.get_workspaces(save=False)
+                self.selected = [False] * len(self.vms)
+                self.stdscr.refresh()
             elif key == ord("l"):  # Toggle logs
                 self.show_logs = not self.show_logs
             elif key == ord("s"):  # SSH into selected VM
-                selected_vms = [vm for i, vm in enumerate(vms) if selected[i]]
+                selected_vms = [vm for i, vm in enumerate(self.vms) if self.selected[i]]
                 if len(selected_vms) == 1:
-                    ssh_to_vm(selected_vms[0])
+                    self.ssh_to_vm(selected_vms[0])
                 elif len(selected_vms) > 1:
                     show_status_message("Please select only one VM for SSH")
                 else:
                     show_status_message("No VM selected for SSH")
             elif key == ord("q"):  # Quit
                 break
-            print_menu()
+            self.print_menu()
+
+    def print_menu(self) -> None:
+        self.stdscr.clear()
+        for idx, vm in enumerate(self.vms):
+            mark = "[*] " if self.selected[idx] else "[ ] "
+            status = "running" if vm.active else "paused"
+            line = (
+                mark + vm.name + f"({status})"
+            )  # Combine the mark and the VM name
+
+            if idx == self.current_row:
+                self.stdscr.addstr(
+                    idx, 0, line, curses.A_REVERSE
+                )  # Highlight the entire line
+            else:
+                self.stdscr.addstr(idx, 0, line)  # Display the line without highlighting
+
+        self.stdscr.addstr(
+            idx + 2,
+            0,
+            "Press \n'j' to move down,\n 'k' to move up,\n'Enter' to select,"
+            "\n 'a' to select all,\n 'p' to pause,\n 'r' to resume,\n 'u' to update status,"
+            "\n's' for ssh access,\n 'l' to toggle logs,\n'q' to quit",
+        )
+        if self.show_logs:
+            self.stdscr.addstr(len(self.vms) + 12, 0, "===logs===")
+            for idx, log in enumerate(self.logs[-10:]):
+                self.stdscr.addstr(len(self.vms) + 13 + idx, 0, log)
+        self.stdscr.refresh()
+
+    def show_status_message(self, message) -> None:
+        self.stdscr.addstr(len(self.vms) + 12, 0, message)
+        self.stdscr.refresh()
+        time.sleep(2)  # Show the message for 2 seconds
+
+    def ssh_to_vm(self, vm):
+        if vm.ip:
+            self.show_status_message(f"Connecting to {vm.name} at {vm.ip}...")
+            ssh_command = f"ssh {vm.ip}"
+
+            try:
+                # Use curses.endwin() to temporarily suspend curses
+                curses.endwin()
+                subprocess.run(ssh_command, shell=True)
+            except Exception as e:
+                logger.error(f"SSH connection failed: {str(e)}")
+            finally:
+                # Reinitialize curses
+                self.stdscr.refresh()
+        else:
+            self.show_status_message(f"No IP address available for {vm.name}")
 
 
 def main():
