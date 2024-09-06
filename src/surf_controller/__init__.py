0d7dc785808e1d234981cf3d05c6b97c2bb6445d
Raoul Grouls <Raoul.Grouls@han.nl>
2024-09-06 10:09:24 +0200

    v0.3.3

--- src/surf_controller/__init__.py
+++ src/surf_controller/__init__.py
@@ -12,10 +12,15 @@ def setup_config():
 
     # If the user's config file doesn't exist, copy the default one
     if not user_config_file.exists():
+        print("You are running surf_controller for the first time.")
+        print("Surfcontroller will guide you through the setup process.")
         default_config = Path(__file__).parent / "config.toml"
         shutil.copy(default_config, user_config_file)
 
 
 setup_config()
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
+print(
+    f"Welcome to surf_controller version {__version__}"
+)
